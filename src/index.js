b0bcfd7c2892f72025f624ffd97f09140b397886
Narek <NSarg7@gmail.com>
2021-03-23 21:58:55 +0400

    Created Header.layout

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import "./configs/axios.config";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+
+    document.getElementById("root")
+);
